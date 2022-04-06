344de4c69b1845c43246672d8b529cc363a6421a
Noah Peden <noah@wespire.com>
2022-04-06 12:44:52 -0400

    semifix: images still buggy on favs and wip

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -6,18 +6,16 @@ import axios from 'axios';
 
 export default function Favorites() {
   const { favorites } = useContext(FavoritesContext);
-  const [spriteImages, setSpriteImages] = useState();
+  const [spriteImages, setSpriteImages] = useState([]);
   const fetchPokemon = async () => {
     try {
-      debugger;
-      console.log(favorites);
       const sprites = favorites.map(async (pokemon) => {
         const sprite = await axios.get(
           `${process.env.REACT_APP_POKEMON_URL}pokemon/${pokemon.name}`
         );
         const spriteImage = sprite?.data?.sprites?.front_default;
-        console.log({ spriteImage });
-        Promise.all(sprites).then((sprites) => setSpriteImages(sprites));
+
+        setSpriteImages([...spriteImages, spriteImage]);
       });
     } catch (e) {
       console.log(e);
@@ -29,17 +27,20 @@ export default function Favorites() {
   }, []);
 
   console.log(spriteImages);
-  return favorites.length > 0 ? (
-    favorites.map((favorite, i) => (
-      <Container sx={{ py: 8 }} maxWidth="md">
-        <Grid container spacing={4}>
-          <Grid item key={favorite.name} xs={12} sm={6} md={4}>
-            <PokeCard spriteImages={[]} pokemon={favorite} index={i} />
+  return favorites.length > 0 && spriteImages.length > 0 ? (
+    favorites.map((favorite, i) => {
+      console.log(spriteImages[i])
+      return (
+        <Container sx={{ py: 8 }} maxWidth="md">
+          <Grid container spacing={4}>
+            <Grid item key={favorite.name} xs={12} sm={6} md={4}>
+              <PokeCard spriteImages={spriteImages} pokemon={favorite} index={i} />
+            </Grid>
           </Grid>
-        </Grid>
-      </Container>
-    ))
+        </Container>
+      );
+    })
   ) : (
-    <Typography>No favs yet</Typography>
+    <Typography align="center">No favs yet</Typography>
   );
 }
